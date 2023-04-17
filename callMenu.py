783605ffe1ac3ec530fd495ded0ab5aab0797d8f
Jay <jay_carlos@dlsu.edu.ph>
2023-04-17 16:35:20 +0800

    fix: delete temp files

--- callMenu.py
+++
@@ -1,9 +0,0 @@
-import json
-import time
-from os import system, name
-
-menuJSON = open('menu.json', 'r')
-menuDatabase = json.load(menuJSON)
-stuffing = menuDatabase['stuffing']
-
-print(str(stuffing[0][0]))
